8b95b8b7571915b45c7069384010bf2efa6ed980
Bongsoon Choi <willbsoon16@gmail.com>
2022-09-30 07:02:39 +0900

    Initial commit

---
+++ src/main/java/com/willbsoon/gc_team1_gathering/GcTeam1GatheringApplication.java
@@ -0,0 +1,13 @@
+package com.willbsoon.gc_team1_gathering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcTeam1GatheringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcTeam1GatheringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/willbsoon/gc_team1_gathering/GcTeam1GatheringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.willbsoon.gc_team1_gathering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcTeam1GatheringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
